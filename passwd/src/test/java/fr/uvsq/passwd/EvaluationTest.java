10d93a69aabef5942ed2ebe8c70350cb28d5094a
Marek Felšöci <marek@felsoci.sk>
2026-04-01 17:35:03 +0200

    Indenter avec 2 espaces

--- passwd/src/test/java/fr/uvsq/passwd/EvaluationTest.java
+++ passwd/src/test/java/fr/uvsq/passwd/EvaluationTest.java
@@ -5,8 +5,8 @@ import static org.junit.jupiter.api.Assertions.assertTrue;
 import org.junit.jupiter.api.Test;
 
 public class EvaluationTest {
-    @Test
-    public void testStrong() {
-        assertTrue(true);
-    }
+  @Test
+  public void testStrong() {
+    assertTrue(true);
+  }
 }
